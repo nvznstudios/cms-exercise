f0f9e7ce52566d35102a09db27bcd4ccd097026b
nvznstudios <mc_2050@live.ca>
2018-10-22 08:49:20 -0400

    Updated functions.php to include eaze-giphy folder

--- _theme/functions.php
+++ _theme/functions.php
@@ -159,3 +159,7 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
+/**
+ * Custom additions by Michael Conte
+ */
+require get_template_directory() . '/inc/eaze-giphy/eaze-giphy-functions.php';
