0c5e22b66ae92e7bc498577dda220a0d329c0361
nvznstudios <mc_2050@live.ca>
2018-10-22 09:10:12 -0400

    Updated footer.php with changes for demo

--- _theme/footer.php
+++ _theme/footer.php
@@ -9,26 +9,59 @@
  * @package eaze-starter
  */
 
-?>
+?>			
+						</div>
+					</div>
+				</div><!-- #content -->
 
-	</div><!-- #content -->
+				<footer id="colophon" class="site-footer">
+					<div class="grid-container">
+						<div class="grid-x grid-padding-x">
+							<div class="cell small-6 medium-3 footer-links">
+								<a href="#">Sign Up</a>
+								<a href="#">Log In</a>
+								<a href="#">Support</a>
+							</div>
+							<div class="cell small-6 medium-3 footer-links">
+								<a href="#">Contact Us</a>
+								<a href="#">Blog</a>
+								<a href="#">Press</a>
+							</div>
+							<div class="cell small-6 medium-3 footer-links">
+								<a href="#">Careers</a>
+								<a href="#">Drivers</a>
+								<a href="#">Licensed Dispensaries</a>
+								<a href="#">Delivery Areas</a>
+							</div>
+							<div class="cell small-6 medium-3 footer-links">
+								<a href="#">Terms and Conditions</a>
+								<a href="#">Privacy Policy</a>
+								<a href="#">Referral Program Rules</a>
+							</div>
+							<div class="cell small-12">
+								<div class="site-info">
+	<!-- 								<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'eaze-starter' ) ); ?>">
+										<?php
+										/* translators: %s: CMS name, i.e. WordPress. */
+										printf( esc_html__( 'Proudly powered by %s', 'eaze-starter' ), 'WordPress' );
+										?>
+									</a>
+									<span class="sep"> | </span>
+										<?php
+										/* translators: 1: Theme name, 2: Theme author. */
+										printf( esc_html__( 'Theme: %1$s by %2$s.', 'eaze-starter' ), 'eaze-starter', '<a href="http://underscores.me/">Underscores.me</a>' );
+										?> -->
+									Copyright © 2014-2018 Eaze Solutions, Inc. All rights reserved.
+								</div><!-- .site-info -->
+							</div>
+						</div>
+					</div>
+				</footer><!-- #colophon -->
+			</div><!-- #page -->
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'eaze-starter' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'eaze-starter' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'eaze-starter' ), 'eaze-starter', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+		</div>
+	</div>
+</div>
 
 <?php wp_footer(); ?>
 
