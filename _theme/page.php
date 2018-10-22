7735deea5f697ee7f97f826a0d20ac56620d2054
nvznstudios <mc_2050@live.ca>
2018-10-22 09:07:14 -0400

    Update page.php with Foundation

--- _theme/page.php
+++ _theme/page.php
@@ -15,6 +15,7 @@
 get_header();
 ?>
 
+<div class="cell small-12">
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
@@ -34,6 +35,7 @@ get_header();
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
+</div>
 
 <?php
 get_sidebar();
